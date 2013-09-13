40c932ba9c3fe725750453eac5b6ed53e32f135e
fh222dt <fh222dt@student.lnu.se>
2013-09-13 10:35:11 +0200

    testa om formuläret är skickat

--- index.php
+++ index.php
@@ -10,28 +10,47 @@
  
  <h1>Laborationskod fh222dt</h1>
  <h2>Ej Inloggad</h2>
-<form class="form-inline">
+<form method="post" action="index.php" class="form-inline">
 <fieldset>
 <legend>Login - Skriv in användarnamn och lösenord</legend>
 <label for="UserName">Användarnamn</label>
-<input id="UserName" type="text" size="15">
+<input id="UserName" name="UserName" type="text" size="15">
 <label for="Password">Lösenord</label>
-<input id="Password" type="password" size="15">
+<input id="Password" name="Password" type="password" size="15">
 
 <label for="KeepLogin" class="checkbox"> 
 	<input id="KeepLogin" type="checkbox"> Håll mig inloggad</label>
 
-<input type="submit" value="Logga in" class="btn">
+<input type="submit" name="submit" value="Logga in" class="btn">
 </fieldset>
 </form>
-</body>
- </html>
+
 
 
 <?php
+/*endast dessa variabler går att logga in med*/
+$username = "Admin";
+$password = "Password";
+
+	
+/*testa om formuläret är skickat*/
+if (isset($_POST['submit'])) {
+
+	echo "HEJA!";
+
+	if ($_POST["UserName"] == $username && $_POST["Password"] == $password) {
+	echo "<br/>jjjjjjjjjjjjjjjjj";
+	}
+
+	else {
+		echo "<br/>Fel användaruppgifter!";
+	}
+}
+
 
 /*Visa datum och tid snyggt på svenska*/
 setlocale (LC_TIME, "Swedish"); 
  echo strftime("%A" . ", den " . "%#d %B %Y" . ". Klockan är [" . "%X" . "]"); //formatet %#d är linux %e
  ?>
- 
+ </body>
+ </html>
